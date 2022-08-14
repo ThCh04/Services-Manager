b5bbc9d623efe5815a3d040dc9e9dbb840223e1a
ThCh! <ThCh!@LAPTOP-6HP1LBO9>
2022-08-13 22:04:10 -0400

    Agregar archivos de proyecto.

---
+++ Services Manager/Form1.Designer.cs
@@ -0,0 +1,421 @@
+﻿
+namespace Services_Manager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.ramTB = new System.Windows.Forms.TextBox();
+            this.cpuTB = new System.Windows.Forms.TextBox();
+            this.pibTB = new System.Windows.Forms.TextBox();
+            this.namTB = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Usage = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.RAM = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.sramTB = new System.Windows.Forms.TextBox();
+            this.scpuTB = new System.Windows.Forms.TextBox();
+            this.sidTB = new System.Windows.Forms.TextBox();
+            this.snamTB = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(837, 615);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.ramTB);
+            this.tabPage1.Controls.Add(this.cpuTB);
+            this.tabPage1.Controls.Add(this.pibTB);
+            this.tabPage1.Controls.Add(this.namTB);
+            this.tabPage1.Controls.Add(this.label7);
+            this.tabPage1.Controls.Add(this.label5);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.panel1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(829, 589);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Processes";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // ramTB
+            // 
+            this.ramTB.Location = new System.Drawing.Point(118, 546);
+            this.ramTB.Name = "ramTB";
+            this.ramTB.ReadOnly = true;
+            this.ramTB.Size = new System.Drawing.Size(100, 20);
+            this.ramTB.TabIndex = 2;
+            // 
+            // cpuTB
+            // 
+            this.cpuTB.Location = new System.Drawing.Point(118, 499);
+            this.cpuTB.Name = "cpuTB";
+            this.cpuTB.ReadOnly = true;
+            this.cpuTB.Size = new System.Drawing.Size(100, 20);
+            this.cpuTB.TabIndex = 2;
+            // 
+            // pibTB
+            // 
+            this.pibTB.Location = new System.Drawing.Point(118, 457);
+            this.pibTB.Name = "pibTB";
+            this.pibTB.ReadOnly = true;
+            this.pibTB.Size = new System.Drawing.Size(100, 20);
+            this.pibTB.TabIndex = 2;
+            // 
+            // namTB
+            // 
+            this.namTB.Location = new System.Drawing.Point(118, 419);
+            this.namTB.Name = "namTB";
+            this.namTB.ReadOnly = true;
+            this.namTB.Size = new System.Drawing.Size(267, 20);
+            this.namTB.TabIndex = 2;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(21, 499);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(66, 13);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "CPU Usage:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(21, 546);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 13);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Ram Usage:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 457);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "PID:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 419);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Process Name:";
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(823, 387);
+            this.panel1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Name,
+            this.ID,
+            this.Usage,
+            this.RAM});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(819, 383);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // Name
+            // 
+            this.Name.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Name.HeaderText = "Name";
+            this.Name.Name = "Name";
+            this.Name.ReadOnly = true;
+            // 
+            // ID
+            // 
+            this.ID.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.ID.HeaderText = "ID";
+            this.ID.Name = "ID";
+            this.ID.ReadOnly = true;
+            // 
+            // Usage
+            // 
+            this.Usage.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Usage.HeaderText = "Usage";
+            this.Usage.Name = "Usage";
+            this.Usage.ReadOnly = true;
+            // 
+            // RAM
+            // 
+            this.RAM.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.RAM.HeaderText = "RAM";
+            this.RAM.Name = "RAM";
+            this.RAM.ReadOnly = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.sramTB);
+            this.tabPage2.Controls.Add(this.scpuTB);
+            this.tabPage2.Controls.Add(this.sidTB);
+            this.tabPage2.Controls.Add(this.snamTB);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.label6);
+            this.tabPage2.Controls.Add(this.label8);
+            this.tabPage2.Controls.Add(this.panel2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(829, 589);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Services";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // sramTB
+            // 
+            this.sramTB.Location = new System.Drawing.Point(118, 556);
+            this.sramTB.Name = "sramTB";
+            this.sramTB.ReadOnly = true;
+            this.sramTB.Size = new System.Drawing.Size(100, 20);
+            this.sramTB.TabIndex = 8;
+            // 
+            // scpuTB
+            // 
+            this.scpuTB.Location = new System.Drawing.Point(118, 509);
+            this.scpuTB.Name = "scpuTB";
+            this.scpuTB.ReadOnly = true;
+            this.scpuTB.Size = new System.Drawing.Size(100, 20);
+            this.scpuTB.TabIndex = 9;
+            // 
+            // sidTB
+            // 
+            this.sidTB.Location = new System.Drawing.Point(118, 467);
+            this.sidTB.Name = "sidTB";
+            this.sidTB.ReadOnly = true;
+            this.sidTB.Size = new System.Drawing.Size(100, 20);
+            this.sidTB.TabIndex = 10;
+            // 
+            // snamTB
+            // 
+            this.snamTB.Location = new System.Drawing.Point(118, 429);
+            this.snamTB.Name = "snamTB";
+            this.snamTB.ReadOnly = true;
+            this.snamTB.Size = new System.Drawing.Size(267, 20);
+            this.snamTB.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 509);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Service Type:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 556);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Status:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(21, 467);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(28, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "SID:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(21, 429);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(77, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Service Name:";
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel2.Controls.Add(this.dataGridView2);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(3, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(823, 387);
+            this.panel2.TabIndex = 3;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn3,
+            this.dataGridViewTextBoxColumn4});
+            this.dataGridView2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView2.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.Size = new System.Drawing.Size(819, 383);
+            this.dataGridView2.TabIndex = 0;
+            this.dataGridView2.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellClick);
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.dataGridViewTextBoxColumn1.HeaderText = "Name";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.dataGridViewTextBoxColumn2.HeaderText = "ID";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            this.dataGridViewTextBoxColumn2.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.dataGridViewTextBoxColumn3.HeaderText = "Type";
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            this.dataGridViewTextBoxColumn3.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.dataGridViewTextBoxColumn4.HeaderText = "Status";
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            this.dataGridViewTextBoxColumn4.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(837, 615);
+            this.Controls.Add(this.tabControl1);
+            this.Text = "S&P Manager";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Usage;
+        private System.Windows.Forms.DataGridViewTextBoxColumn RAM;
+        private System.Windows.Forms.TextBox ramTB;
+        private System.Windows.Forms.TextBox cpuTB;
+        private System.Windows.Forms.TextBox pibTB;
+        private System.Windows.Forms.TextBox namTB;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox sramTB;
+        private System.Windows.Forms.TextBox scpuTB;
+        private System.Windows.Forms.TextBox sidTB;
+        private System.Windows.Forms.TextBox snamTB;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+    }
+}
+
---
+++ Services Manager/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+using System.ServiceProcess;
+using System.Management;
+using System.Dynamic;
+
+namespace Services_Manager
+{
+    public partial class Form1 : Form
+    {
+        
+        uint processId = 0,sid=0;
+        dynamic rm;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //EVENTOS PARA OBTENER LOS VALORES DE LOS DATAGRIDVIEW EN LAS PESTAÑAS DE PROCESOS Y SERVICIOS
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //SELECCIONANDO EL ÍNDICE Y PASANDO LO VALORES DE LOS PROCESOS
+            int get = e.RowIndex;
+            DataGridViewRow rows = dataGridView1.Rows[get];
+            namTB.Text = rows.Cells[0].Value.ToString();
+            pibTB.Text = rows.Cells[1].Value.ToString();
+            cpuTB.Text = rows.Cells[2].Value.ToString();
+            ramTB.Text = rows.Cells[3].Value.ToString();
+        }
+
+        private void dataGridView2_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //SELECCIONANDO EL ÍNDICE Y PASANDO LO VALORES DE LOS SERVICIOS
+            int get = e.RowIndex;
+            DataGridViewRow rows = dataGridView2.Rows[get];
+            snamTB.Text = rows.Cells[0].Value.ToString();
+            sidTB.Text = rows.Cells[1].Value.ToString();
+            scpuTB.Text = rows.Cells[2].Value.ToString();
+            sramTB.Text = rows.Cells[3].Value.ToString();
+        }
+
+        //OBTENIENDO EL PID DE CADA SERVICIO CON OBJECTSEARCHER (RETORNA UN UINT CON EL VALOR DEL PID DEL SERVICIO ACTUAL)
+        uint GetProcessIDByServiceName(string serviceName)
+        {
+            
+            string qry = "SELECT PROCESSID FROM WIN32_SERVICE WHERE NAME = '" + serviceName + "'";
+            System.Management.ManagementObjectSearcher searcher = new System.Management.ManagementObjectSearcher(qry);
+            foreach (System.Management.ManagementObject mngntObj in searcher.Get())
+            {
+                processId = (uint)mngntObj["PROCESSID"];
+            }
+            return processId;
+        }
+
+     
+
+        //MOSTRANDO LA INFORMACIÓN SOLICITADA
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            //PRIMERA TAREA (BUSCA LOS PROCESOS Y SU INFORMACIÓN Y LA MUESTRA EN EL DATAGRIDVIEW DE PROCESOS)
+            var task1 = new Task(() =>
+            {
+                Thread.Sleep(0);
+                CheckForIllegalCrossThreadCalls = false; //PARA PODER ATRAPAR LAS ASIGNACIONES ERRONEAS DEL CONTROL AL THREAD
+                Process[] proc = Process.GetProcesses();
+
+                //RECORRIENDO LOS PROCESOS, SACANDO LA INFORMACIÓN Y AÑADIENDOLA AL DATAGRIDVIEW PROCESOS
+            foreach (Process p in proc)
+            {
+                PerformanceCounter percentP = new PerformanceCounter("Process", "% Processor Time", p.ProcessName, true);
+                percentP.NextValue();
+
+                dataGridView1.Rows.Add(p.ProcessName, p.Id,(( Math.Round( percentP.NextValue(),0))/100 * .100)*100+"%"/*REDONDEANDO EL PORCENTAJE CPU*/, p.WorkingSet64 / 1048576+"MB");
+                   
+                }
+               
+
+            });
+            //INICIANDO TAREA 1
+            task1.Start();
+
+            //SEGUNDA TAREA (BUSCA LOS SERVICIOS Y SU INFORMACIÓN Y LA MUESTRA EN EL DATAGRIDVIEW DE PROCESOS)
+            var task2 = new Task(() =>
+            {
+                Thread.Sleep(0);
+                CheckForIllegalCrossThreadCalls = false;
+                ServiceController[] serv = ServiceController.GetServices();
+
+                //RECORRIENDO LOS SERVICIOS, SACANDO LA INFORMACIÓN Y AÑADIENDOLA AL DATAGRIDVIEW SERVICIOS
+                foreach (ServiceController s in serv)
+                {
+                    sid = GetProcessIDByServiceName(s.ServiceName);//OBTENIENDO EL PID DEL SERVICIO
+                    dataGridView2.Rows.Add(s.ServiceName,sid,s.ServiceType, s.Status);
+
+                }
+
+            });
+            //INICIANDO TAREA 2
+            task2.Start();
+        }
+    }
+}
